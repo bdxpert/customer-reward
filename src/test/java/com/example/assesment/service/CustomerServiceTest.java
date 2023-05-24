802166f793b67a5ed36a295776480953f503f605
Sanjiv Kumar Sarkar <sanjiv771@gmail.com>
2023-05-23 20:35:49 -0500

    Test case updated

--- src/test/java/com/example/assesment/service/CustomerServiceTest.java
+++ src/test/java/com/example/assesment/service/CustomerServiceTest.java
@@ -1,44 +1,51 @@
 package com.example.assesment.service;
 
+import com.example.assesment.domain.IReward;
+import com.example.assesment.domain.Reward;
 import com.example.assesment.dto.CustomerRewardDTO;
 import com.example.assesment.entity.Customer;
 import com.example.assesment.entity.TransactionRecord;
 import com.example.assesment.repository.CustomerRepository;
-import com.example.assesment.repository.TransactionRecordRepository;
+import com.example.assesment.service.impl.CustomerServiceImpl;
 import com.example.assesment.util.Utility;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 //https://docs.spring.io/spring-boot/docs/2.1.2.RELEASE/reference/htmlsingle/#boot-features-testing-spring-boot-applications
 @SpringBootTest
 public class CustomerServiceTest {
 
-    @Autowired
+
     CustomerService customerService;
-    @Autowired
+    ModelMapper modelMapper;
     CustomerRepository customerRepository;
-    @Autowired
-    TransactionRecordRepository transactionRecordRepository;
+    IReward iReward;
+
     private Customer customer;
 
 
     @BeforeEach
     public void setup() {
+        iReward = new Reward();
+        customerRepository = mock(CustomerRepository.class);
+        modelMapper = new ModelMapper();
+        customerService = new CustomerServiceImpl(customerRepository, modelMapper, iReward);
+
         TransactionRecord one = new TransactionRecord(200L, LocalDate.now(), 120.0);
-        transactionRecordRepository.save(one);
         TransactionRecord two = new TransactionRecord(202L, LocalDate.now().minusMonths(1), 40);
-        transactionRecordRepository.save(two);
         TransactionRecord three = new TransactionRecord(203L, LocalDate.now().minusMonths(2), 80.0);
-        transactionRecordRepository.save(three);
 
         List<TransactionRecord> transactionRecordList = new ArrayList<>();
         transactionRecordList.add(one);
@@ -54,13 +61,16 @@ public class CustomerServiceTest {
     @DisplayName("Test customer reward calculation")
     @Test
     void calculateReward() {
-        Customer customer1 = customerRepository.save(customer);
-        CustomerRewardDTO customerDTO = customerService.getCustomerReward(customer1.getId(), Utility.getSearchStartDate(), Utility.getSearchEndDate());
+        when(customerRepository.findAllByIdAndTransactionRecordsDateBetween(customer.getId(), Utility.getSearchStartDate(), Utility.getSearchEndDate())).thenAnswer(ans->{
+            return Optional.of(customer);
+        });
+
+        CustomerRewardDTO customerDTO = customerService.getCustomerReward(customer.getId(), Utility.getSearchStartDate(), Utility.getSearchEndDate());
 
         assertEquals(120, customerDTO.getReward().getTotalRewardPoint());
-        assertEquals(90, customerDTO.getReward().getFirstMonthRewardPoint());
+        assertEquals(30, customerDTO.getReward().getFirstMonthRewardPoint());
         assertEquals(0, customerDTO.getReward().getSecondMonthRewardPoint());
-        assertEquals(30, customerDTO.getReward().getThirdMonthRewardPoint());
+        assertEquals(90, customerDTO.getReward().getThirdMonthRewardPoint());
     }
 
 }
